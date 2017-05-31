5a30e08f754efe8b4ec2fd6ec1f3d9cfe70da884
Thayanne <thayannevls@gmail.com>
2017-05-31 03:33:46 -0300

    create classes

--- system/FaceDetector.py
+++ system/FaceDetector.py
@@ -1,4 +1,14 @@
 import cv2
-import openface
+import numpy as np
 
 class FaceDetector(object):
+
+    def __init__(self):
+        self.facecascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.detector = dlib.get_frontal_face_detector()
+
+    def detectFaces(self, image):
+        image = cv2.imread(image)
+        image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        
---
+++ system/FaceRecognizer.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import dlib
+import openface
+
+class FaceRecognizer(object):
+
+    def __init__(self):
+
+    def recognize(self, image):
+        
