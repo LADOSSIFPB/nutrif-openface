76b0fa8c773d0351d44eb7db7cd5c3f5ba671f94
thayannevls <thayannevls@gmail.com>
2017-06-21 16:55:18 -0300

    update

--- system/App.py
+++ system/App.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template
+from flask_cors import CORS
+from JsonUtils import convert_input_to, json_repr
 from flask_socketio import SocketIO, emit
 import FaceDetector as fd
-import FaceRecognizer as fz
+from Aluno import Aluno
+
 app = Flask(__name__)
+CORS(app)
+
+@app.route("/detect", methods=['POST'])
+@convert_input_to(Aluno)
+def detectFaces(aluno):
+
+    # Recuperar Imagem
+    print("mat:" + aluno.matricula)
+    #print("imagem:" + aluno.imagem)
+
+    # detector = fd.FaceDetector()
+    # return detector.detectFaces(image)
+
+    return "{'retorno':'true'}", 200
 
 
-@app.route("/detect", methods=['GET', 'POST'])
-def detectFaces(image):
-    detector = fd.FaceDetector()
-    return detector.detectFaces(image)
-
-@app.route("/recognize", methods=['GET', 'POST'])
-def recognizeFace(face):
-    return fz.recognize(face)
+@app.route("/train", methods=['GET', 'POST'])
+def train():
+    return ""
 
 if __name__ == "__main__":
     app.run(
