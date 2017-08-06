6bcd6446af5f301647385271b65ff6b478cd9432
Thayanne <thayannevls@gmail.com>
2017-08-06 14:48:18 -0300

    remove comments

--- system/FaceDetector.py
+++ system/FaceDetector.py
@@ -7,7 +7,6 @@ class FaceDetector(object):
         self.faceCascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
 
     def detectFaces(self, image):
-        # faceCascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
         img = cv2.imread(image)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.faceCascade.detectMultiScale(gray, 1.3, 5)
